a931c1e951a2d230a27d5c8392e9acfd3a033294
samuel <samueltandoh4u@gmail.com>
2024-07-02 11:10:21 +0000

    commit

---
+++ myapp/src/components/LoginRegister.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './LoginRegister.css';
+import { FaUser,  FaLock } from "react-icons/fa";
+
+const LoginRegister = () => {
+    return(
+        <div className="wrapper">
+            <div className="form-box login">
+                <form action="">
+                    <h1>Login</h1>
+                    <div className="input box">
+                        <input type="text" placeholder="Email or Username" required/>
+                        <FaUser className="icon" />
+                    </div>
+                    <div className="input box">
+                        <input type="password" placeholder="Password" required/>
+                        <FaLock className="icon" />
+                    </div>
+
+                    <div className="remember-forgot">
+                        <label><input type="checkbox" />Remember me</label>
+                        <a href="#">Forgot password?</a>
+                    </div>
+
+                    <button type="submit">Login</button>
+                    <div className="register-link">
+                        <p>Don't have an account? <a href="#">Register</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginRegister;

b1d348c5d3c9b57b1e54ef43e04d1486ce667f44
samuel <samueltandoh4u@gmail.com>
2024-07-14 12:17:48 +0000

    commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import LoginRegister from './components/LoginRegister';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <div>
      <LoginRegister/>
+     </div>
+     <div>
+      <Navbar/>
+     </div>
     </div>
   );
 }
---
+++ myapp/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+    <div className='logo'>
+        Men 
+    </div>
+    <ul className='navbar-menu'>
+        <li><a href="/">Tools</a></li>
+        <li><a href="/">API</a></li>
+        <li><a href="/">Pricing</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Contact</a></li>
+        <li><a href="/">Register/Login</a></li>
+    </ul>
+    </div>
+  )
+}
+
+export default Navbar
